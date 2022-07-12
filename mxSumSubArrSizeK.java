21a62895d459b40b1e9a6f3548cb13701b794535
Varis Rana <vrvarisrana123@gmail.com>
2022-07-12 06:30:30 +0530

    commit 12

--- mxSumSubArrSizeK.java
+++ mxSumSubArrSizeK.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 public class mxSumSubArrSizeK {
-      
-      public static void main(String[] args) {
 
+      public static void main(String[] args) {
+            int N = 4, K = 2;
+            int[] arr = { 100, 200, 300, 400 };
+            
       }
 }
