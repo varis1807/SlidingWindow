5c5a38eb77f41367690bac2a0ddc6b8fc8a5230b
Varis Rana <vrvarisrana123@gmail.com>
2022-07-12 10:17:44 +0530

    commit 12

--- firstNegIntKGrp.java
+++ firstNegIntKGrp.java
@@ -2,27 +2,27 @@ import java.util.*;
 public class firstNegIntKGrp {
       public long[] printFirstNegativeInteger(long A[], int N, int k)
       {
-          int i=0;
-          int j=0;
+          int si=0;
+          int ei=0;
           int z=0;
           ArrayList<Integer> list=new ArrayList<Integer>();
           long[] ans=new long[N-k+1];
-          while(j<N){
+          while(ei<N){
             
-              if(A[j]<0){
-                  list.add(j);
+              if(A[ei]<0){
+                  list.add(ei);
               }
-              if(j-i+1<k){
-                  j++;
-              }else if(j-i+1==k){
+              if(ei-si+1<k){
+                  ei++;
+              }else if(ei-si+1==k){
                   if(list.size()==0){
                      ans[z++]=0; 
                   }else if(list.size()>0){
                       ans[z++]=A[list.get(0)];
                   }
-                  i++;
-                  j++;
-                    if(list.size()>0 && list.get(0)<i){
+                  si++;
+                  ei++;
+                    if(list.size()>0 && list.get(0)<si){
                   list.remove(list.get(0));
               }
               }
